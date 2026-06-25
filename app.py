d91ae59c0eb5804b0613b96a49e11964debebf8e
Tody0224 <tody0224@gmail.com>
2026-06-25 02:59:25 -0300

    fix: try login deploy

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_login import LoginManager, current_user
+from werkzeug.middleware.proxy_fix import ProxyFix
 from models.user import Usuario
 from models.character import CharacterModel
 from controllers.route_controller import bp
 from datetime import datetime
 import sqlite3
+import os
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-app.secret_key = 'super_secret_rpg_key_123'
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+app.secret_key = os.environ.get('SECRET_KEY', 'chave_padrao_local')
 
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_REFRESH_EACH_REQUEST'] = True
@@ -42,7 +46,6 @@ login_manager.init_app(app)
 def load_user(user_id):
     return Usuario.get_by_id(int(user_id))
 
-app.register_blueprint(bp)
-
+app.register_blueprint(bp, url_prefix='/')
 if __name__ == '__main__':
     app.run(debug=True)
